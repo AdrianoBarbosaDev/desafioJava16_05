52834f8b4da604922f6ceca63721705e729b6ce0
AdrianoBarbosaDev <adrianobarbosa1999@gmail.com>
2025-05-22 21:25:33 -0300

    complete all tasks

--- src/App.java
+++ src/App.java
@@ -3,9 +3,12 @@ import model.Pessoa;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -72,9 +75,73 @@ public class App {
             System.out.println("Função: " + funcao);
             listaFuncionarios.forEach(f -> System.out.println(f));
         });
+        System.out.println("******************************************************");
+        System.out.println("INICIANDO ITEM 3.6- AGRUPANDO FUNCIONARIOS POR FUNCAO:");
 
+        List<Funcionario> listaOrdenadaFuncionarios = funcionarios.stream().sorted(Comparator.comparing(Funcionario::getFuncao)).collect(Collectors.toList());
+        listaOrdenadaFuncionarios.forEach(f -> System.out.println(f));
 
         System.out.println("******************************************************");
+
+        // funcionarios.forEach(
+        //     f -> f.getDataNascimento().getDayOfMonth() = 10 || 12 ? System.out.println(f) : System.out.println("num deu")
+        // );
+
+        System.out.println("******************************************************");
+        System.out.println("INICIANDO ITEM 3.8- IMPRIMINDO FUNCIONARIOS QUE NASCERAM NOS MESES 10 E 12:");
+        for (Funcionario funcionario: funcionarios){
+            int mesNascimento = funcionario.getDataNascimento().getMonthValue();
+            if(mesNascimento==10 || mesNascimento==12){
+                System.out.println(funcionario);
+            }
+        }
+        System.out.println("******************************************************");
+
+        System.out.println("******************************************************");
+        System.out.println("INICIANDO ITEM 3.9- IMPRIMINDO O FUNCIONARIO COM MAIOR IDADE:");
+        //  List<Funcionario> listaOrdenadaFuncionariosIdade = funcionarios.stream().sorted(Comparator.comparing(Funcionario::getDataNascimento)).collect(Collectors.toList());
+        // listaOrdenadaFuncionariosIdade.forEach(f -> System.out.println(f));
+
+        Funcionario funcionarioMaisAntigo = new Funcionario(null, LocalDate.now(), null, null);
+        for(Funcionario funcionario : funcionarios){
+            if(funcionario.getDataNascimento().isBefore(funcionarioMaisAntigo.getDataNascimento())){
+                funcionarioMaisAntigo = funcionario;
+            }
+        }
+        System.out.println("Nome: " +funcionarioMaisAntigo.getNome() + " Idade: " + Period.between(funcionarioMaisAntigo.getDataNascimento(), LocalDate.now()).getYears());
+
+        System.out.println("******************************************************");
+
+        System.out.println("******************************************************");
+        System.out.println("INICIANDO ITEM 3.10- IMPRIMINDO FUNCIONARIOS ORDENADOS ALFABETICAMENTE:");
+        List<Funcionario> listaOrdenadaFuncionariosNome = funcionarios.stream().sorted(Comparator.comparing(Funcionario::getNome)).collect(Collectors.toList());
+        listaOrdenadaFuncionariosNome.forEach(f -> System.out.println(f));
+
+        System.out.println("******************************************************");
+
+        System.out.println("******************************************************");
+        System.out.println("INICIANDO ITEM 3.11- IMPRIMINDO TOTAL DE SALARIOS:");
+
+        double totalSalario = 0;
+        for (Funcionario funcionario : funcionarios){
+            totalSalario += funcionario.getSalario().doubleValue();
+        }
+
+        System.out.println(totalSalario);
+
+        System.out.println("******************************************************");
+
+        System.out.println("******************************************************");
+        System.out.println("INICIANDO ITEM 3.12- IMPRIMINDO SALARIOS MINIMOS POR FUNCIONARIO:");
+
+        BigDecimal salarioMinimo = new BigDecimal("1212.00");
+        for (Funcionario f : funcionarios) {
+            BigDecimal qtdSalarios = f.getSalario()
+                .divide(salarioMinimo, 2, RoundingMode.HALF_UP);
+
+            System.out.println("Funcionário(a) " + f.getNome() + " recebe " + qtdSalarios + " salários mínimos.");
+        }
+
 
     }
 }
